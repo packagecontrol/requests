587a156a6003907051a4d1039d8383ea7b46a27b
FichteFoll <fichtefoll2@googlemail.com>
2015-03-01 22:51:55 +0100

    Remove loader.py, it's not needed anymore
    The 'all' folder has been added to the lookup dirs in PC 3.0.1.

--- loader.py
+++
@@ -1,7 +0,0 @@
-import os.path
-
-from package_control import sys_path
-
-dependency_dir = os.path.dirname(sys_path.generate_dependency_paths('requests')['ver'])
-
-sys_path.add(os.path.join(dependency_dir, 'all'))
